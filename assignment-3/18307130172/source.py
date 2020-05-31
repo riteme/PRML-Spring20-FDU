e1111fe21ee2436efd8d5e0907388f5144bbda38
riteme <riteme@qq.com>
2020-05-31 22:35:16 +0800

    submission of assignment-3 of 18307130172

--- assignment-3/18307130172/source.py
+++ assignment-3/18307130172/source.py
@@ -1,29 +1,35 @@
 from typing import List, Tuple
 
 import argparse
+import itertools
 
 import numpy as np
 import scipy as sp
+import scipy.stats
 import matplotlib.pyplot as plt
 
 Vector = Tuple[float, float]
 Covariance = List[List[float]]
 
-PRIORS = [0.3, 0.2, 0.5]
+PRIORS = [0.3, 0.2, 0.4, 0.1]
 MEANS = [
     (-0.5, -0.6),
     (0.6, -0.4),
-    (0.1, 0.3)
+    (0.1, 0.3),
+    (0.0, -0.3)
 ]
-COVS = [[
+COVS = [[  # must be symmetric & positive-semidefinite
     [0.03, 0],
     [0, 0.03]
 ], [
-    [0.04, 0.03],
-    [0.01, 0.05]
+    [0.04, -0.02],
+    [-0.02, 0.05]
 ], [
     [0.07, 0.04],
-    [0.01, 0.06]
+    [0.04, 0.06]
+], [
+    [0.005, 0],
+    [0, 0.005]
 ]]
 
 def plot_data(data: np.ndarray, **kwargs):
@@ -36,9 +42,12 @@ def generate_data(
 ) -> np.ndarray:
     cnt = np.random.multinomial(n, prior)
     data = np.vstack([
-        np.random.multivariate_normal(
-            mean[i], cov[i], cnt[i]
-        )
+        np.hstack((
+            np.random.multivariate_normal(
+                mean[i], cov[i], cnt[i]
+            ),
+            i * np.ones(cnt[i]).reshape(-1, 1)
+        ))
         for i in range(len(prior))
     ])
     np.random.shuffle(data)
@@ -61,7 +70,9 @@ def k_means(
     k: int, steps: int,
     debug=False
 ) -> np.ndarray:
-    p = np.random.normal(scale=0.5, size=(k, 2))
+    # p = np.random.normal(scale=0.5, size=(k, 2))
+    # choose data points to avoid NaN.
+    p = data[np.random.choice(len(data), k)]
 
     for _ in range(steps):
         label = get_label_by_distance(data, p, k)
@@ -132,7 +143,7 @@ def expectation_maximize(
         pi = cnt / cnt.sum()
         mu = (gamma.transpose() @ data) / cnt.reshape(-1, 1)
 
-        # ...
+        # broadcasting is evil...
         diff = data[np.newaxis, ...] - mu[:, np.newaxis, :]
         diff = diff[..., np.newaxis]
         result = (diff @ diff.transpose(0, 1, 3, 2)).transpose(1, 0, 2, 3)
@@ -141,12 +152,76 @@ def expectation_maximize(
 
     return pi, mu, sigma
 
-def main(args):
-    pass
+def cli_generate(args):
+    data = generate_data(args.number, PRIORS, MEANS, COVS)
+    with open(args.output, 'w') as fp:
+        np.savetxt(fp, data[:, 0:2])
+    with open(f'{args.output}.label', 'w') as fp:
+        np.savetxt(fp, data[:, 2], fmt='%.0f')
+
+def cli_run(args):
+    with open(args.data, 'r') as fp:
+        data = np.loadtxt(fp)
+
+    pi, mu, sigma = expectation_maximize(
+        data, args.K, args.kstep, args.estep, args.verbose
+    )
+    _, label = get_label_by_guassian(data, pi, mu, sigma, args.K)
+
+    print(f'μ = {mu}')
+    print(f'π = {pi}')
+
+    if args.accuracy:
+        with open(f'{args.data}.label', 'r') as fp:
+            std = np.loadtxt(fp, dtype=np.int)
+
+        # since label name may be different, enumerate all
+        # possible remappings to get the right accuracy.
+        accuracy = 0
+        for perm in itertools.permutations(range(args.K)):
+            remap = label.copy()
+            for i in range(args.K):
+                remap[label == i] = perm[i]
+
+            result = std == remap
+            val = np.count_nonzero(result) / len(result)
+            accuracy = max(accuracy, val)
+
+        print(f'Accuracy: {accuracy * 100}%')
+
+    if args.plot:
+        plt.xlim(-1.1, 1.1)
+        plt.ylim(-1.1, 1.1)
+        for i in range(args.K):
+            plot_data(data[label == i], alpha=0.25)
+        plot_data(mu, marker='x', color='red')
+        plt.show()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--number', type=int, help='Number of data points.')
-    args = parser.parse_args()
+    subparser = parser.add_subparsers()
 
-    main(args)
+    generate_parser = subparser.add_parser('generate', help='Generate data.')
+    generate_parser.set_defaults(handler=cli_generate)
+    generate_parser.add_argument(
+        '-n', '--number', type=int, default=100,
+        help='Number of data points.'
+    )
+    generate_parser.add_argument(
+        '-o', '--output', type=str, default='fake.data',
+        help='File where data will be saved.'
+    )
+
+    run_parser = subparser.add_parser('run', help='Run EM algorithm.')
+    run_parser.set_defaults(handler=cli_run)
+    run_parser.add_argument('data', type=str, help='File path of the data.')
+    run_parser.add_argument('-k', '--kstep', type=int, default=10, help='Number of steps for K-means algorithm.')
+    run_parser.add_argument('-e', '--estep', type=int, default=30, help='Number of steps for EM algorithm.')
+    run_parser.add_argument('-v', '--verbose', action='store_true', help='Show steps.')
+    run_parser.add_argument('-p', '--plot', action='store_true', help='Show result wuth pyplot.')
+    run_parser.add_argument('-a', '--accuracy', action='store_true', help='Calculate accuracy.')
+    run_parser.add_argument('-K', type=int, default=3, help='Number of clusters.')
+
+    args = parser.parse_args()
+    if hasattr(args, 'handler'):
+        args.handler(args)
